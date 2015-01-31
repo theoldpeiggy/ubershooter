4ffb989a0029de31e8888bf309e73c9245f937cb
Peihong Li <peihong.li@shopify.com>
2015-01-31 00:57:33 -0500

    set up controller, view and routes

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,8 @@
+class GameController < ApplicationController
+
+  #This is going to be the default page for the application, used for EVERYTHING
+  def play
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  root 'game#play'
+
+  get 'play', to: 'game#play'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
