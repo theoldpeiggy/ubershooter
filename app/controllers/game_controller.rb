7b9c06dc2423ad27ae0637b30010dbb4408ffa82
Peihong Li <peihong.li@shopify.com>
2015-01-31 19:10:15 -0500

    real time requests

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,11 +1,20 @@
 class GameController < ApplicationController
+  respond_to :json, only: [:ledOn, :ledOff]
 
   #This is going to be the default page for the application, used for EVERYTHING
   def play
 
   end
 
-  def hit
+  def ledOn
+    edison = Edison.new
+    edison.ledOn
+    respond_with success: true
+  end
 
+  def ledOff
+    edison = Edison.new
+    edison.ledOff
+    respond_with success: true
   end
 end
